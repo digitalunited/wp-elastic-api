b8a99ef0aaca9be870270e759103bae477da1c75
mien <mikael.engstrom@careofhaus.se>
2014-10-08 11:05:45 +0200

    resolves exception in search and adds example config and ignore original'

--- src/WPElasticAPI/Application.php
+++ src/WPElasticAPI/Application.php
@@ -4,123 +4,128 @@ namespace DigitalUnited\WPElasticAPI;
 
 class Application {
 
-	public $slim;
-	public $elastica;
+    public $slim;
+    public $elastica;
 
-	public function __construct( \Slim\Slim $slim = null ) {
-		$this->slim     = ! empty( $slim ) ? $slim : \Slim\Slim::getInstance( $this->getInstanceName() );
-		$this->elastica = new \Elastica\Client( array(
-			'host' => $this->getElasticsearchHost(),
-			'port' => $this->getElasticsearchPort()
-		) );
-	}
+    public function __construct( \Slim\Slim $slim = null ) {
+        $this->slim     = ! empty( $slim ) ? $slim : \Slim\Slim::getInstance( $this->getInstanceName() );
+        $this->elastica = new \Elastica\Client( array(
+            'host' => $this->getElasticsearchHost(),
+            'port' => $this->getElasticsearchPort()
+        ) );
+    }
 
-	public function getElasticsearchIndex() {
-		return Configuration::get( 'index' );
-	}
+    public function getElasticsearchIndex() {
+        return Configuration::get( 'index' );
+    }
 
-	public function getElasticsearchHost() {
-		return Configuration::get( 'host' );
-	}
+    public function getElasticsearchHost() {
+        return Configuration::get( 'host' );
+    }
 
-	public function getElasticsearchPort() {
-		return Configuration::get( 'port' );
-	}
+    public function getElasticsearchPort() {
+        return Configuration::get( 'port' );
+    }
 
-	public function getInstanceName() {
-		return Application::InstanceName();
-	}
+    public function getInstanceName() {
+        return Application::InstanceName();
+    }
 
-	public static function InstanceName() {
-		return Configuration::get( 'instance_name' );
-	}
+    public static function InstanceName() {
+        return Configuration::get( 'instance_name' );
+    }
 
-	public static function BasePath() {
-		return Configuration::get( 'base_path' );
-	}
+    public static function BasePath() {
+        return Configuration::get( 'base_path' );
+    }
 
-	public function getBasePath() {
-		return Application::BasePath();
-	}
+    public function getBasePath() {
+        return Application::BasePath();
+    }
 
-	public function getBodyAsArray() {
-		$body = $this->slim->request()->getBody();
-		$body = json_decode( $body );
-		if ( is_object( $body ) ) {
-			$body = get_object_vars( $body );
-		}
+    public function getBodyAsArray() {
+        $body = $this->slim->request()->getBody();
+        $body = json_decode( $body );
 
-		return $body;
-	}
+        if ( !$body ) {
+            return array();
+        }
 
-	public function blockInvalidIPAddress() {
-		if ( ! in_array( $this->slim->request()->getIp(), $this->getValidIPAddresses() ) ) {
-			throw new \Exception( 'Request made from invalid IP-address: ' . $this->slim->request()->getIp() );
-		}
-	}
+        if ( is_object( $body ) ) {
+            $body = get_object_vars( $body );
+        }
 
-	public function getValidIPAddresses() {
+        return $body;
+    }
 
-		return Configuration::get( 'valid_ip' );
+    public function blockInvalidIPAddress() {
+        if ( ! in_array( $this->slim->request()->getIp(), $this->getValidIPAddresses() ) ) {
+            throw new \Exception( 'Request made from invalid IP-address: ' . $this->slim->request()->getIp() );
+        }
+    }
 
-	}
+    public function getValidIPAddresses() {
+
+        return Configuration::get( 'valid_ip' );
+
+    }
 
 
-	public function check_req_opt_param( $rparam = array(), $optparam = array(), $uparam = array() ) {
+    public function check_req_opt_param( $rparam = array(), $optparam = array(), $uparam = array() ) {
 
-		if ( ! is_array( $rparam )
-		     || ! is_array( $optparam )
-		     || ! is_array( $uparam )
-		) {
+        if ( ! is_array( $rparam )
+             || ! is_array( $optparam )
+             || ! is_array( $uparam )
+        ) {
 
-			throw new \Exception( 'Parameters has to be arrays.' );
-		}
+            throw new \Exception( 'Parameters has to be arrays.' );
+        }
 
-		//Final User i/p array
-		$uparam_final_array = array();
+        //Final User i/p array
+        $uparam_final_array = array();
 
-		//Checking if req parameter exsist in user parameter
-		for ( $i = 0; $i < sizeof( $rparam ); $i ++ ) {
+        //Checking if req parameter exsist in user parameter
+        for ( $i = 0; $i < sizeof( $rparam ); $i ++ ) {
 
-			$req_param_chk = array_key_exists( $rparam[ $i ], $uparam );
+            $req_param_chk = array_key_exists( $rparam[ $i ], $uparam );
 
-			//If Key Dont Exsist
-			if ( $req_param_chk == false ) {
-				throw new \Exception( "Required Parameter: '" . $rparam[ $i ] . "' is missing" );
-			} else { //If Key Present
-				// Checking If not set
-				if ( ! isset( $uparam[ $rparam[ $i ] ] ) ) {
-					throw new \Exception( "Parameter: '" . $rparam[ $i ] . "' - must be specified" );
-				} else { //if Not empty
-					$uparam_final_array[ $rparam[ $i ] ] = $uparam[ $rparam[ $i ] ];
-					unset( $uparam[ $rparam[ $i ] ] );
-				}
-			}
-		}
+            //If Key Dont Exsist
+            if ( $req_param_chk == false ) {
+                throw new \Exception( "Required Parameter: '" . $rparam[ $i ] . "' is missing" );
+            } else { //If Key Present
+                // Checking If not set
+                if ( ! isset( $uparam[ $rparam[ $i ] ] ) ) {
+                    throw new \Exception( "Parameter: '" . $rparam[ $i ] . "' - must be specified" );
+                } else { //if Not empty
+                    $uparam_final_array[ $rparam[ $i ] ] = $uparam[ $rparam[ $i ] ];
+                    unset( $uparam[ $rparam[ $i ] ] );
+                }
+            }
+        }
 
-		// Optional Parameter Check
-		for ( $j = 0; $j < sizeof( $optparam ); $j ++ ) {
-			$optl_param_chk = array_key_exists( $optparam[ $j ], $uparam );
-			//If Key Dont Exsist
-			if ( $optl_param_chk == false ) {
-				$uparam_final_array[ $optparam[ $j ] ] = null;
-			} else { //if Key Present
-				//Checking if empty
-				if ( empty( $uparam[ $optparam[ $j ] ] ) ) {
-					$uparam_final_array[ $optparam[ $j ] ] = null;
-				} else { //if Not empty
-					$uparam_final_array[ $optparam[ $j ] ] = $uparam[ $optparam[ $j ] ];
-				}
-				unset( $uparam[ $optparam[ $j ] ] );
-			}
-		}
+        // Optional Parameter Check
+        for ( $j = 0; $j < sizeof( $optparam ); $j ++ ) {
+            $optl_param_chk = array_key_exists( $optparam[ $j ], $uparam );
+            //If Key Dont Exsist
+            if ( $optl_param_chk == false ) {
+                $uparam_final_array[ $optparam[ $j ] ] = null;
+            } else { //if Key Present
+                //Checking if empty
+                if ( empty( $uparam[ $optparam[ $j ] ] ) ) {
+                    $uparam_final_array[ $optparam[ $j ] ] = null;
+                } else { //if Not empty
+                    $uparam_final_array[ $optparam[ $j ] ] = $uparam[ $optparam[ $j ] ];
+                }
+                unset( $uparam[ $optparam[ $j ] ] );
+            }
+        }
 
-		// Checking for invalid user parameters
-		if ( sizeof( $uparam ) >= 1 ) {
-			throw new \Exception( 'Invalid parameters for this command (' . join( ',', array_keys( $uparam ) ) . ')' );
-		}
+        // Checking for invalid user parameters
+        if ( sizeof( $uparam ) >= 1 ) {
+            throw new \Exception( 'Invalid parameters for this command (' . join( ',', array_keys( $uparam ) ) . ')' );
+        }
 
-	}
+    }
 
 
 }
--- src/WPElasticAPI/Configuration.php
+++ src/WPElasticAPI/Configuration.php
@@ -7,75 +7,75 @@ namespace DigitalUnited\WPElasticAPI;
  * and checking permissions
  */
 class Configuration {
-	/**
-	 * @var StdObj
-	 */
-	protected static $_configuration;
+    /**
+     * @var StdObj
+     */
+    protected static $_configuration;
 
-	/**
-	 * Overwrites keys if wpbase-prefixed constant is defined
-	 * Ex. WPBASE_FOOBAR overwrites the foobar-property
-	 *
-	 * @param mixed $configuration Configuration array
-	 *
-	 * @return array Updated configuration array
-	 */
-	protected static function apply_user_overwrites( $configuration ) {
-		foreach ( $configuration as $param => $val ) {
-			$const_name = 'WP_ELASTIC_API_' . strtoupper( $param );
+    /**
+     * Overwrites keys if wpbase-prefixed constant is defined
+     * Ex. WPBASE_FOOBAR overwrites the foobar-property
+     *
+     * @param mixed $configuration Configuration array
+     *
+     * @return array Updated configuration array
+     */
+    protected static function apply_user_overwrites( $configuration ) {
+        foreach ( $configuration as $param => $val ) {
+            $const_name = 'WP_ELASTIC_API_' . strtoupper( $param );
 
-			if ( defined( $const_name ) ) {
-				$configuration[ $param ] = constant( $const_name );
-			}
-		}
+            if ( defined( $const_name ) ) {
+                $configuration[ $param ] = constant( $const_name );
+            }
+        }
 
-		return $configuration;
-	}
+        return $configuration;
+    }
 
-	/**
-	 * If a configuration exists it will be overwritten
-	 *
-	 * @param $configuration_name mixed config-file available in config folder, default: dev/prod dependent on wp_debug
-	 */
-	public static function load_configuration( $configuration_name = false ) {
-		$configuration_folder = __DIR__ . '/config/';
-		if ( $configuration_name ) {
-			$file_name = $configuration_name . '.php';
-		} else {
-			$file_name = WP_DEBUG ? 'development.php' : 'production.php';
-		}
-		$full_path = $configuration_folder . $file_name;
+    /**
+     * If a configuration exists it will be overwritten
+     *
+     * @param $configuration_name mixed config-file available in config folder, default: dev/prod dependent on wp_debug
+     */
+    public static function load_configuration( $configuration_name = false ) {
+        $configuration_folder = __DIR__ . '/config/';
+        if ( $configuration_name ) {
+            $file_name = $configuration_name . '.php';
+        } else {
+            $file_name = defined('WP_DEBUG') && WP_DEBUG ? 'development.php' : 'production.php';
+        }
+        $full_path = $configuration_folder . $file_name;
 
-		if ( ! is_readable( $full_path ) ) {
-			throw new \Exception( sprintf( "File %s could not be read", $full_path ) );
-		}
+        if ( ! is_readable( $full_path ) ) {
+            throw new \Exception( sprintf( "File %s could not be read", $full_path ) );
+        }
 
-		$default_config = include $full_path;
+        $default_config = include $full_path;
 
-		self::$_configuration = (object) self::apply_user_overwrites( $default_config );
+        self::$_configuration = (object) self::apply_user_overwrites( $default_config );
 
-		return $default_config;
-	}
+        return $default_config;
+    }
 
-	/**
-	 * @param mixed $property requested configuration property
-	 *
-	 * @return mixed StdObj|string
-	 */
-	public static function get( $property = false ) {
-		if ( ! self::$_configuration ) {
-			self::load_configuration();
-		}
+    /**
+     * @param mixed $property requested configuration property
+     *
+     * @return mixed StdObj|string
+     */
+    public static function get( $property = false ) {
+        if ( ! self::$_configuration ) {
+            self::load_configuration();
+        }
 
-		if ( $property ) {
-			if ( self::$_configuration->$property ) {
-				return self::$_configuration->$property;
-			} else {
-				throw new \Exception( sprintf( 'Property %s do not exist', $property ) );
-			}
-		}
+        if ( $property ) {
+            if ( self::$_configuration->$property ) {
+                return self::$_configuration->$property;
+            } else {
+                throw new \Exception( sprintf( 'Property %s do not exist', $property ) );
+            }
+        }
 
-		return self::$_configuration;
-	}
+        return self::$_configuration;
+    }
 
 }
