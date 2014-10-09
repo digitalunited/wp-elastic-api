f31db88ecd4fad7b6b8c6dd3ae5d956952bc2519
mien <mikael.engstrom@careofhaus.se>
2014-10-09 16:42:44 +0200

    added filters

--- src/WPElasticAPI/Posts.php
+++ src/WPElasticAPI/Posts.php
@@ -11,254 +11,277 @@ namespace DigitalUnited\WPElasticAPI;
  */
 class Posts extends \DigitalUnited\WPElasticAPI\Application {
 
-	static function createInstance( $app ) {
-		$app->container->singleton('Posts', function () {
-			return new Posts();
-		});
-		$app->Posts->routes( $app );
-	}
+    static function createInstance( $app ) {
+        $app->container->singleton('Posts', function () {
+            return new Posts();
+        });
+        $app->Posts->routes( $app );
+    }
 
-	function routes( \Slim\Slim $app ) {
+    function routes( \Slim\Slim $app ) {
 
-		$base = $this->getBasePath();
+        $base = $this->getBasePath();
 
-		$app->post( $base . '/posts/search', function () use ( $app ) {
-			$app->Posts->search();
-		} );
+        $app->post( $base . '/posts/search', function () use ( $app ) {
+            $app->Posts->search();
+        } );
 
-		$app->post( $base . '/posts/:id', function ( $id ) use ( $app ) {
-			$app->Posts->getById( $id );
-		} );
+        $app->post( $base . '/posts/:id', function ( $id ) use ( $app ) {
+            $app->Posts->getById( $id );
+        } );
 
-		$app->get( $base . '/posts/:id', function ( $id ) use ( $app ) {
-			$app->Posts->getById( $id );
-		} );
+        $app->get( $base . '/posts/:id', function ( $id ) use ( $app ) {
+            $app->Posts->getById( $id );
+        } );
 
-		$app->post( $base . '/posts', function () use ( $app ) {
-			$app->Posts->save();
-		} );
+        $app->post( $base . '/posts', function () use ( $app ) {
+            $app->Posts->save();
+        } );
 
-		$app->delete( $base . '/posts/:id', function ( $id ) use ( $app ) {
-			$app->Posts->delete( $id );
-		} );
+        $app->delete( $base . '/posts/:id', function ( $id ) use ( $app ) {
+            $app->Posts->delete( $id );
+        } );
 
-	}
+    }
 
-	/**
-	 * @SWG\Api(
-	 *   path="/posts",
-	 *   @SWG\Operation(
-	 *     method="POST",
-	 *     summary="Create or update a post and returns the data[] if success. This action requires valid IP-address.",
-	 *     nickname="posts_save",
-	 * 		@SWG\Parameter(
-	 *           name="post_type",
-	 *           required=true,
-	 *           type="string"
-	 *         ),
-	 * 		@SWG\Parameter(
-	 *           name="ID",
-	 *           required=true,
-	 *           type="integer"
-	 *         ),
-	 * 		@SWG\Parameter(
-	 *           name="data",
-	 *           required=true,
-	 *           type="array[]"
-	 *         )
-	 *     )
-	 * )
-	 */
-	function save() {
+    /**
+     * @SWG\Api(
+     *   path="/posts",
+     *   @SWG\Operation(
+     *     method="POST",
+     *     summary="Create or update a post and returns the data[] if success. This action requires valid IP-address.",
+     *     nickname="posts_save",
+     *      @SWG\Parameter(
+     *           name="post_type",
+     *           required=true,
+     *           type="string"
+     *         ),
+     *      @SWG\Parameter(
+     *           name="ID",
+     *           required=true,
+     *           type="integer"
+     *         ),
+     *      @SWG\Parameter(
+     *           name="data",
+     *           required=true,
+     *           type="array[]"
+     *         )
+     *     )
+     * )
+     */
+    function save() {
 
-		$this->blockInvalidIPAddress();
+        $this->blockInvalidIPAddress();
 
-		$body   = $this->getBodyAsArray();
-		$required_parameters = array( 'post_type', 'ID','data' );
-		$optional_parameters = array();
-		$this->check_req_opt_param( $required_parameters , $optional_parameters , $body );
+        $body   = $this->getBodyAsArray();
+        $required_parameters = array( 'post_type', 'ID','data' );
+        $optional_parameters = array();
+        $this->check_req_opt_param( $required_parameters , $optional_parameters , $body );
 
-		$index = $this->elastica->getIndex( $this->getElasticsearchIndex() );
-		$type  = $index->getType( $body['post_type'] );
+        $index = $this->elastica->getIndex( $this->getElasticsearchIndex() );
+        $type  = $index->getType( $body['post_type'] );
 
-		$doc = new \Elastica\Document( $body['ID'], json_encode( $body['data'] ) );
-		$type->addDocument( $doc );
+        $doc = new \Elastica\Document( $body['ID'], json_encode( $body['data'] ) );
+        $type->addDocument( $doc );
 
-		$index->refresh();
+        $index->refresh();
 
-		echo json_encode( $body['data'] );
+        echo json_encode( $body['data'] );
 
-	}
+    }
 
-	/**
-	 * @SWG\Api(
-	 *   path="/posts/:id",
-	 *   @SWG\Operation(
-	 *     method="DELETE",
-	 *     summary="Deletes a post and returns true if success. This action requires valid IP-address.",
-	 *     nickname="posts_delete",
-	 * 		@SWG\Parameter(
-	 *           name="ID",
-	 *           required=true,
-	 *           type="integer"
-	 *         )
-	 * )
-	 * )
-	 */
-	function delete( $id ) {
+    /**
+     * @SWG\Api(
+     *   path="/posts/:id",
+     *   @SWG\Operation(
+     *     method="DELETE",
+     *     summary="Deletes a post and returns true if success. This action requires valid IP-address.",
+     *     nickname="posts_delete",
+     *      @SWG\Parameter(
+     *           name="ID",
+     *           required=true,
+     *           type="integer"
+     *         )
+     * )
+     * )
+     */
+    function delete( $id ) {
 
-		$this->blockInvalidIPAddress();
+        $this->blockInvalidIPAddress();
 
-		$body                = $this->getBodyAsArray();
-		$required_parameters = array( 'post_type' );
-		$optional_parameters = array();
-		$this->check_req_opt_param( $required_parameters, $optional_parameters, $body );
+        $body                = $this->getBodyAsArray();
+        $required_parameters = array( 'post_type' );
+        $optional_parameters = array();
+        $this->check_req_opt_param( $required_parameters, $optional_parameters, $body );
 
-		$index = $this->elastica->getIndex( $this->getElasticsearchIndex() );
-		$type  = $index->getType( $body['post_type'] );
+        $index = $this->elastica->getIndex( $this->getElasticsearchIndex() );
+        $type  = $index->getType( $body['post_type'] );
 
-		$type->deleteById( $id );
-		$index->refresh();
+        $type->deleteById( $id );
+        $index->refresh();
 
-		echo json_encode( true );
+        echo json_encode( true );
 
-	}
+    }
 
-	/**
-	 * @SWG\Api(
-	 *   path="/posts/search",
-	 *   @SWG\Operation(
-	 *     method="POST",
-	 *     summary="Search items in posts",
-	 *     nickname="posts_search",
-	 * 		@SWG\Parameter(
-	 *           name="post_type",
-	 *           required=false,
-	 *           type="string"
-	 *         ),
-	 * 		@SWG\Parameter(
-	 *           name="search_phrase",
-	 *           required=false,
-	 *           type="string"
-	 *         ),
-	 * 		@SWG\Parameter(
-	 *           name="sort",
-	 *           description="sort configuration, eg [{'post_name':{'order':'asc'}}]",
-	 *           required=false,
-	 *           type="array[]"
-	 *         ),
-	 * 		@SWG\Parameter(
-	 *           name="limit",
-	 *           description="Max number of documents in return",
-	 *           required=false,
-	 *           type="integer"
-	 *         )
-	 *   )
-	 * )
-	 */
-	function search() {
+    /**
+     * @SWG\Api(
+     *   path="/posts/search",
+     *   @SWG\Operation(
+     *     method="POST",
+     *     summary="Search items in posts",
+     *     nickname="posts_search",
+     *      @SWG\Parameter(
+     *           name="post_type",
+     *           required=false,
+     *           type="string"
+     *         ),
+     *      @SWG\Parameter(
+     *           name="filters",
+     *           description="filter values, eg. [{my_parameter: 'mustmatchthis'}]
+     *           required=false,
+     *           type="array[]"
+     *         ),
+     *      @SWG\Parameter(
+     *           name="search_phrase",
+     *           required=false,
+     *           type="string"
+     *         ),
+     *      @SWG\Parameter(
+     *           name="sort",
+     *           description="sort configuration, eg [{'post_name':{'order':'asc'}}]",
+     *           required=false,
+     *           type="array[]"
+     *         ),
+     *      @SWG\Parameter(
+     *           name="limit",
+     *           description="Max number of documents in return",
+     *           required=false,
+     *           type="integer"
+     *         )
+     *   )
+     * )
+     */
+    function search() {
 
-		$result = array();
+        $result = array();
 
-		$body   = $this->getBodyAsArray();
-		$required_parameters = array();
-		$optional_parameters = array( 'post_type', 'search_phrase', 'sort', 'limit' );
-		$this->check_req_opt_param( $required_parameters , $optional_parameters , $body );
+        $body   = $this->getBodyAsArray();
+        $required_parameters = array();
+        $optional_parameters = array( 'post_type', 'filters', 'search_phrase', 'sort', 'limit' );
+        $this->check_req_opt_param( $required_parameters , $optional_parameters , $body );
 
-		$index = $this->elastica->getIndex( $this->getElasticsearchIndex() );
+        $index = $this->elastica->getIndex( $this->getElasticsearchIndex() );
 
-		$query = new \Elastica\Query();
-		$boolean = new \Elastica\Query\Bool();
-		$added = false;
+        $query = new \Elastica\Query();
+        $boolean = new \Elastica\Query\Bool();
+        $added = false;
 
-		$type  = isset( $body['post_type'] ) ? $body['post_type'] : null;
-		if ( $type ) {
-			$q = new \Elastica\Query\Term( array( "_type" => $type ) );
-			$boolean->addMust( $q );
-			$added = true;
-		}
+        $type  = isset( $body['post_type'] ) ? $body['post_type'] : null;
+        if($type) {
+            $q = new \Elastica\Query\Term(array('_type' => $type));
+            $boolean->addMust($q);
+            $added = true;
+        }
 
-		$search_phrase  = isset( $body['search_phrase'] ) ? $body['search_phrase'] : null;
-		if ( isset( $search_phrase ) && ! empty( $search_phrase ) ) {
-			$word                = strtolower( $search_phrase ) . '*';
-			$elasticaQueryString = new \Elastica\Query\SimpleQueryString( $word );
-			$boolean->addMust( $elasticaQueryString );
-			$added = true;
-		}
+        $filters = isset($body['filters']) ? (array)$body['filters'] : array();
+        $filters = array_filter($filters);
 
-		if ( $added ) {
-			$query->setQuery( $boolean );
-		}
+        $filterAnd = '';
+        if ($filters) {
+            $filterAnd = new \Elastica\Filter\BoolAnd();
+            foreach($filters as $key => $val) {
+                $filter = new \Elastica\Filter\Term();
+                $filter->setTerm($key, $val);
+                $filterAnd->addFilter($filter);
+            }
+        }
 
-		$limit  = isset( $body['limit'] ) ? (int)$body['limit'] : null;
-		if( $limit ) {
-			$query->setSize( $limit );
-		}
+        $search_phrase  = isset( $body['search_phrase'] ) ? $body['search_phrase'] : null;
+        if ( isset( $search_phrase ) && ! empty( $search_phrase ) ) {
+            $word                = strtolower( $search_phrase ) . '*';
+            $elasticaQueryString = new \Elastica\Query\SimpleQueryString( $word );
+            $boolean->addMust( $elasticaQueryString );
+            $added = true;
+        }
 
-		$sort  = isset( $body['sort'] ) ? $body['sort'] : null;
-		if($sort ) {
-			$query->setSort( $sort ); // example: array( 'post_date' => array( 'order' => 'desc' ) )
-		}
+        if ( $added ) {
+            $query->setQuery( $boolean );
+        }
 
-		$elasticaResultSet = $index->search( $query );
+        if ($filterAnd) {
+            $query->setFilter($filterAnd);
+        }
 
-		$elasticaResults = $elasticaResultSet->getResults();
+        $limit  = isset( $body['limit'] ) ? (int)$body['limit'] : null;
+        if( $limit ) {
+            $query->setSize( $limit );
+        }
 
-		foreach ( $elasticaResults as $elasticaResult ) {
-			$result[] = $elasticaResult->getData();
-		}
+        $sort  = isset( $body['sort'] ) ? $body['sort'] : null;
+        if($sort ) {
+            $query->setSort( $sort ); // example: array( 'post_date' => array( 'order' => 'desc' ) )
+        }
 
-		echo json_encode( $result );
+        $elasticaResultSet = $index->search( $query );
 
-	}
+        $elasticaResults = $elasticaResultSet->getResults();
 
-	/**
-	 * @SWG\Api(
-	 *   path="/posts/:id",
-	 *   @SWG\Operation(
-	 *     method="GET",
-	 *     summary="Gets a post",
-	 *     nickname="posts_get"
-	 *   )
-	 * )
-	 */
-	function getById( $id ) {
+        foreach ( $elasticaResults as $elasticaResult ) {
+            $result[] = $elasticaResult->getData();
+        }
 
-		$result = array();
+        echo json_encode( $result );
 
-		$body   = $this->getBodyAsArray();
-		$required_parameters = array();
-		$optional_parameters = array( 'post_type' );
-		$this->check_req_opt_param( $required_parameters , $optional_parameters , $body );
+    }
 
-		$index = $this->elastica->getIndex( $this->getElasticsearchIndex() );
+    /**
+     * @SWG\Api(
+     *   path="/posts/:id",
+     *   @SWG\Operation(
+     *     method="GET",
+     *     summary="Gets a post",
+     *     nickname="posts_get"
+     *   )
+     * )
+     */
+    function getById( $id ) {
 
-		$query = new \Elastica\Query();
-		$boolean = new \Elastica\Query\Bool();
-		$added = false;
+        $result = array();
 
-		$type  = isset( $body['post_type'] ) ? $body['post_type'] : null;
-		if ( $type ) {
-			$q = new \Elastica\Query\Term( array( "_type" => $type ) );
-			$boolean->addMust( $q );
-		}
+        $body   = $this->getBodyAsArray();
+        $required_parameters = array();
+        $optional_parameters = array( 'post_type' );
+        $this->check_req_opt_param( $required_parameters , $optional_parameters , $body );
 
-		$q = new \Elastica\Query\Term( array( "_id" => $id ) );
-		$boolean->addMust( $q );
+        $index = $this->elastica->getIndex( $this->getElasticsearchIndex() );
 
-		$query->setQuery( $boolean );
-		$query->setSize( 1 );
+        $query = new \Elastica\Query();
+        $boolean = new \Elastica\Query\Bool();
+        $added = false;
 
-		$elasticaResultSet = $index->search( $query );
+        $type  = isset( $body['post_type'] ) ? $body['post_type'] : null;
+        if ( $type ) {
+            $q = new \Elastica\Query\Term( array( "_type" => $type ) );
+            $boolean->addMust( $q );
+        }
 
-		$elasticaResults = $elasticaResultSet->getResults();
+        $q = new \Elastica\Query\Term( array( "_id" => $id ) );
+        $boolean->addMust( $q );
 
-		foreach ( $elasticaResults as $elasticaResult ) {
-			$result[] = $elasticaResult->getData();
-		}
+        $query->setQuery( $boolean );
+        $query->setSize( 1 );
 
-		echo json_encode( $result );
+        $elasticaResultSet = $index->search( $query );
 
-	}
+        $elasticaResults = $elasticaResultSet->getResults();
+
+        foreach ( $elasticaResults as $elasticaResult ) {
+            $result[] = $elasticaResult->getData();
+        }
+
+        echo json_encode( $result );
+
+    }
 
 }
